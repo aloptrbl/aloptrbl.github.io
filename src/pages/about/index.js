b5e844ddf1ad4ddcac8fa38f2a96bcaf7fb0a021
aloptrbl <thatzelo@gmail.com>
2024-03-07 14:27:41 +0800

    Update index.js

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -13,7 +13,7 @@ export default function About() {
 <h1>Introduction</h1>
 <hr></hr>
     <p>Hello, I am a mobile developer with over 4 years of experience. <br></br> I specialize in native mobile development, with a focus on Kotlin for Android and Swift & SwiftUI for iOS. My goal is to create applications that not only satisfy the end-users but also bring joy to fellow developers during the development process.</p>
-
+    <br></br>
     <h2>Skills</h2>
     <hr></hr>
     <ul>
@@ -22,7 +22,7 @@ export default function About() {
         <li>React Native</li>
         <li>CakePHP, Node JS, React</li>
     </ul>
-
+    <br></br>
     <h2>Projects</h2>
     <hr></hr>
     <h3>Qalby App</h3>
@@ -36,7 +36,7 @@ export default function About() {
 
 <h3>FASSPOS app (In flight app Air Asia)</h3>
 <p>an onboard retail solution for Air Asia, enabling crews to manage inventory, scan QR codes, and accept contactless payments.</p>
-
+<br></br>
     <h2>Education</h2>
     <hr></hr>
     <p>Bachelor of Computer Science (Software Development): Graduated from UTeM, Melaka in 2020.</p>
