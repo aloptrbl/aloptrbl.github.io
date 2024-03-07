dd18799183ad9ee3623417966fc992a32125a6bb
aloptrbl <thatzelo@gmail.com>
2024-03-07 23:18:09 +0800

    add stroke

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -10,20 +10,37 @@ export default function About() {
   return (
     <Layout>
 <h1>Introduction</h1>
+<br></br>
 <hr></hr>
-    <p>Hello, I am a mobile developer & web developer with over 4 years of experience. <br></br> I specialize in native mobile development, with a focus on Kotlin for Android and Swift & SwiftUI for iOS. My goal is to create applications that not only satisfy the end-users but also bring joy to fellow developers during the development process.</p>
+    <p>   Hello, I am a mobile developer & web developer with over 4 years of experience. <br></br> I specialize in native mobile development, with a focus on Kotlin for Android and Swift & SwiftUI for iOS. My goal is to create applications that not only satisfy the end-users but also bring joy to fellow developers during the development process.</p>
     <br></br>
     <h2>Skills</h2>
+    <br></br>
     <hr></hr>
-    <ul>
+    <ul className="list-disc px-3">
         <li>Kotlin & Jetpack Compose</li>
         <li>Swift & SwiftUI</li>
         <li>React Native</li>
         <li>CakePHP, Node JS, React</li>
-        <li>MYSQL, GraphQL, Firebase</li>
+        <li>Espresso</li>
+        <li>Circle CI</li>
+        <li>MYSQL, GraphQL, Firebase, RESTful APIs</li>
+    </ul>
+    <br></br>
+    <h2>Tools</h2>
+    <br></br>
+    <hr></hr>
+    <ul className="list-disc px-3">
+      <li>Postman</li>
+      <li>Android Studio, XCode, VSCode</li>
+      <li>Android SDK, Cocoapods, Gradle</li>
+      <li>Git, Github, Gitlab</li>
+      <li>Figma</li>
+      <li>Trello</li>
     </ul>
     <br></br>
     <h2>Projects</h2>
+    <br></br>
     <hr></hr>
     <h3>Qalby App</h3>
     <p>Qalby App is a comprehensive Islamic mobile application designed to provide trusted Islamic essentials, education, and lifestyle content based on the teachings of the Qur’an and Sunnah</p>
@@ -37,7 +54,7 @@ export default function About() {
     <h3>Wahdah App</h3>
     <p> It’s designed for fleet management and has several features.</p>
     <br></br>
-<h3>FASSPOS app (In flight app Air Asia)</h3>
+<h3>FASSPOS App (In flight app Air Asia)</h3>
 <p>an onboard retail solution for Air Asia, enabling crews to manage inventory, scan QR codes, and accept contactless payments.</p>
 <br></br>
 <h3>Renorumah.com Web App</h3>
@@ -47,6 +64,7 @@ export default function About() {
 <p>Qalby Ilm finder is a feature of the Qalby that helps users find Islamic classes & events. Meanwhile, Qalby Community is a platform where users can share and read daily inspirations, watch curated Islamic reminders.</p>
 <br></br>
     <h2>Education</h2>
+    <br></br>
     <hr></hr>
     <p>Bachelor of Computer Science (Software Development): Graduated from UTeM, Melaka in 2020.</p>
     </ Layout>
